2f6eb9b58a76a83147d54bd759b49818966b66bb
Gwan-gyeong Mun <elongbug@gmail.com>
2017-09-17 14:25:35 +0900

    egl/tizen: add EGL_NATIVE_SURFACE_TIZEN target of eglCreateImageKHR()
    It adds TIZEN_image_native_surface extension string to _EGLExtensions.
    And it adds a routine of creating an EGLImage from a tbm_surface.
    
      - section overview
        from https://www.khronos.org/registry/EGL/extensions/TIZEN/EGL_TIZEN_image_native_surface.txt
    
        "Tizen Buffer Manager (TBM) is a user space, generic memory management
         framework to create and share memory buffers between different system
         components. This extension enables using a Tizen Buffer Manager (TBM)
         surface object (struct tbm_surface_h) as an EGLImage source."
    
    Signed-off-by: Mun Gwan-gyeong <elongbug@gmail.com>

--- src/egl/main/eglapi.c
+++ src/egl/main/eglapi.c
@@ -536,6 +536,8 @@ _eglCreateExtensionsString(_EGLDisplay *dpy)
 
    _EGL_CHECK_EXTENSION(NV_post_sub_buffer);
 
+   _EGL_CHECK_EXTENSION(TIZEN_image_native_surface);
+
    _EGL_CHECK_EXTENSION(WL_bind_wayland_display);
    _EGL_CHECK_EXTENSION(WL_create_wayland_buffer_from_image);
 
