ca6a9a240682fead4107e954bd752e047070bb92
Gwan-gyeong Mun <elongbug@gmail.com>
2017-10-03 06:11:05 -0700

    egl: add dri2_egl_surface_get_image_front() helper (v2)
    To share common get and create dri_image_front code.
    
    In preparation to adding of new platform which uses this helper.
    
    v2:
     - Remove unneeded ifdef magic
     - Fixes from Eric's review:
       a) Split out series of refactor for helpers to a separate series.
       b) Add the new helper function and use them to replace the old code in the
          same patch.
    
    Signed-off-by: Mun Gwan-gyeong <elongbug@gmail.com>

--- src/egl/drivers/dri2/egl_dri2.h
+++ src/egl/drivers/dri2/egl_dri2.h
@@ -478,6 +478,9 @@ dri2_egl_surface_destroy_image_back(struct dri2_egl_surface *dri2_surf);
 void
 dri2_egl_surface_destroy_image_front(struct dri2_egl_surface *dri2_surf);
 
+int
+dri2_egl_surface_get_image_front(struct dri2_egl_surface *dri2_surf,
+                                 unsigned int format);
 EGLBoolean
 dri2_init_surface(_EGLSurface *surf, _EGLDisplay *dpy, EGLint type,
         _EGLConfig *conf, const EGLint *attrib_list, EGLBoolean enable_out_fence);
